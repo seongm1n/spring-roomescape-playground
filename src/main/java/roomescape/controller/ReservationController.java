ca8289c57134d3233919e4b9dc29889e9cc9fa10
seongmin <0411tjdals34@gmail.com>
2025-02-04 23:21:26 +0900

    fix : DTO 삭제

--- src/main/java/roomescape/controller/ReservationController.java
+++ src/main/java/roomescape/controller/ReservationController.java
@@ -3,7 +3,6 @@ package roomescape.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
-import roomescape.domain.dto.ReservationDto;
 import roomescape.domain.entity.Reservation;
 
 import java.time.LocalDate;
@@ -13,10 +12,13 @@ import java.util.List;
 
 @Controller
 public class ReservationController {
-    private final List<Reservation> reservations;
+    private final List<Reservation> reservations = new ArrayList<>();
 
     public ReservationController() {
-        this.reservations = new ArrayList<>();
+        initializeTestData();
+    }
+
+    private void initializeTestData() {
         reservations.add(new Reservation(1L, "브라운", LocalDate.of(2023, 1, 1), LocalTime.of(10, 0)));
         reservations.add(new Reservation(2L, "브라운", LocalDate.of(2023, 1, 2), LocalTime.of(11, 0)));
         reservations.add(new Reservation(3L, "브라운", LocalDate.of(2023, 1, 3), LocalTime.of(12, 0)));
@@ -29,9 +31,7 @@ public class ReservationController {
 
     @GetMapping("/reservations")
     @ResponseBody
-    public List<ReservationDto> getReservations() {
-        return reservations.stream()
-                .map(ReservationDto::from)
-                .toList();
+    public List<Reservation> getReservations() {
+        return reservations;
     }
 }
