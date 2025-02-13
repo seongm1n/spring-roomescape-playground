5b3410b9ae768fb565f7913451fc0e1e3212c3a5
seongmin <0411tjdals34@gmail.com>
2025-02-13 15:48:09 +0900

    refactor: TIME 자료형 수정

--- src/main/java/roomescape/domain/entity/Time.java
+++ src/main/java/roomescape/domain/entity/Time.java
@@ -1,12 +1,10 @@
 package roomescape.domain.entity;
 
 import java.time.LocalTime;
-import java.time.format.DateTimeFormatter;
 
 public class Time {
     private final Long id;
     private final LocalTime time;
-    private static final DateTimeFormatter TIME_FORMATTER = DateTimeFormatter.ofPattern("HH:mm");
 
     public Time(Long id, LocalTime time) {
         this.id = id;
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -3,6 +3,6 @@ CREATE TABLE reservation
     id      BIGINT       NOT NULL AUTO_INCREMENT,
     name    VARCHAR(255) NOT NULL,
     `date`  DATE         NOT NULL,
-    `time`  VARCHAR(255) NOT NULL,
+    `time`  TIME         NOT NULL,
     PRIMARY KEY (id)
 );
